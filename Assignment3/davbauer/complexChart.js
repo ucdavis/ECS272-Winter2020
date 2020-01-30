142ec488ded7190962b7cf5090dd26c884fb3025
David Bauer <davbauer@ucdavis.edu>
2020-01-29 18:43:38 -0800

    assignment3

---
+++ Assignment3/davbauer/complexChart.js
@@ -0,0 +1,230 @@
+class ComplexChart {
+    constructor(data, html_root, dimensions) {
+        this.keys = null
+        this.data = this.transformData(data)
+
+        this.html_root = html_root
+
+        this.width = dimensions.width
+        this.height = dimensions.height
+        this.margin = dimensions.margin
+
+        this.x = null
+        this.y = null
+        this.xAxis = null
+        this.yAxis = null
+        this.plot_function = null
+        this.colors = null
+
+        this.init()
+    }
+
+    transformData(data) {
+        this.keys = d3.set(data, d => {
+            return d.Terminal
+        }).values()
+
+        var nested = d3.nest()
+        .key(d => { return new Date(d.ReportPeriod) })
+        .rollup(v => { 
+            var result = {}
+            this.keys.forEach(key => {
+                result[key] = d3.sum(v, d => { 
+                    return d.Terminal == key ? parseInt(d.Passenger_Count) : 0 
+                })
+            })
+            return result
+        })
+        .entries(data)
+
+        return nested.map(d => {
+            d.value.date = new Date(d.key)
+            return d.value
+        })
+    }
+
+    init() {
+        // Clear the tag
+        d3.select(this.html_root + " > *").remove()
+
+        // Add new items
+        var svg = d3.select(this.html_root)
+        .append('svg')
+            .attr('width', this.width + this.margin.left + this.margin.right)
+            .attr('height', this.height + this.margin.top + this.margin.bottom) 
+
+        var view = svg.append("g")
+            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")")
+    
+    
+        //scale functions
+        this.x = d3.scaleTime()
+            .domain([d3.min(this.data, d => d.date), d3.max(this.data, d => d.date)])
+            .range([0, this.width])
+            
+        this.y = d3.scaleLinear()
+            .range([this.height, 0])
+
+        // axis
+        this.xAxis = d3.axisBottom(this.x).ticks(6)
+        this.yAxis = d3.axisLeft(this.y).ticks(6)
+
+        // plot function and colors
+        this.plot_function = d3.area()
+            .x(d => { return this.x(d.data.date) })
+            .y0(d => { return this.y(d[0]) })
+            .y1(d => { return this.y(d[1]) })
+
+        this.colors = d3.scaleOrdinal()
+        .domain(this.keys)
+        .range(d3.schemeSet2)
+
+        // stream graph
+        var stack = d3.stack()
+            .offset(d3.stackOffsetSilhouette)
+            .keys(this.keys)
+            (this.data)
+
+        var ymin = d3.min(stack.map(d => {
+            return d3.min(d.map(v => {
+                return d3.min(v)
+            }))
+        }))
+        var ymax = d3.max(stack.map(d => {
+            return d3.max(d.map(v => {
+                return d3.max(v)
+            }))
+        }))
+        this.y.domain([ymin*1.1, ymax*1.1])
+
+        // create the streamgraph
+        var self = this;
+        view.selectAll(".layer")
+            .data(stack)
+            .enter()
+            .append("path")
+            .attr("class", "plot")
+            .style("fill", d => { return this.colors(d.key); })
+            .attr("d", this.plot_function)
+            .on("mouseover", function(d) { self.onMouseOver(self, this, d) })
+            .on("mousemove", (d, i) => this.onMouseMove(d, i))
+            .on("mouseleave", (d) => this.onMouseLeave(d))
+    
+        // x axis
+        view.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + this.height + ")")
+            .call(this.xAxis)
+            .append("text")
+            .attr("fill", "#000")
+            .attr("x", this.width / 2)
+            .attr('y', this.margin.bottom / 2)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Time")
+    
+        // y axis
+        view.append("g")
+            .attr("class", "y axis")
+            .call(this.yAxis)
+            .append("text")
+            .attr("fill", "#000")
+            .attr("transform", "rotate(-90)")
+            .attr("x", - this.height / 2.5 )
+            .attr("y", - this.margin.left)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Passengers per Terminal")
+
+        // terminal hover text
+        view.append("text")
+            .attr("class", "tooltip")
+            .attr("x", 20)
+            .attr("y", 20)
+            .style("opacity", 0)
+            .style("font-size", 17)
+    }
+
+    onMouseOver(self, el, d) {
+        d3.select(self.html_root).selectAll(".tooltip")
+            .style("opacity", 1)
+        d3.select(self.html_root).selectAll(".plot")
+            .style("opacity", 0.5)
+        d3.select(el)
+            .style("stroke", "black")
+            .style("opacity", 1)
+    }
+
+    onMouseMove(d, i) {
+        d3.selectAll(this.html_root).selectAll(".tooltip").text(this.keys[i])
+    }
+
+    onMouseLeave(d) {
+        d3.selectAll(this.html_root).selectAll(".tooltip")
+            .style("opacity", 0)
+        d3.selectAll(this.html_root).selectAll(".plot")
+            .style("opacity", 1)
+            .style("stroke", "none")
+    }
+
+    updateChart(data) {
+        console.log("Primary Chart Entries: " + data.length)
+
+        // Re-calculate data
+        this.data = this.transformData(data)
+        var stack = d3.stack()
+            .offset(d3.stackOffsetSilhouette)
+            .keys(this.keys)
+            (this.data)
+
+
+        // Update axis
+        this.x.domain([d3.min(this.data, d => d.date), d3.max(this.data, d => d.date)])
+        var ymin = d3.min(stack.map(d => {
+            return d3.min(d.map(v => {
+                return d3.min(v)
+            }))
+        }))
+        var ymax = d3.max(stack.map(d => {
+            return d3.max(d.map(v => {
+                return d3.max(v)
+            }))
+        }))
+        this.y.domain([ymin*1.1, ymax*1.1])
+        d3.select(this.html_root).select(".x.axis")
+            .transition()
+            .duration(500)
+            .call(this.xAxis)
+        d3.select(this.html_root).select(".y.axis")        
+            .transition()
+            .duration(500)
+            .call(this.yAxis)
+
+        // Update chart
+        var layers = d3.select(this.html_root).selectAll("path")
+        .data(stack)
+
+
+        layers
+            .exit()
+            .transition()
+            .duration(500)
+            .remove();
+
+        layers
+            .enter()
+            .append("path")
+            .attr("class", "plot")
+            .style("fill", d => { return this.colors(d.key); })
+            .attr("d", this.plot_function)
+            .on("mouseover", function(d) { self.onMouseOver(self, this, d) })
+            .on("mousemove", (d, i) => this.onMouseMove(d, i))
+            .on("mouseleave", (d) => this.onMouseLeave(d))
+
+        layers
+            .transition()
+            .duration(500)
+            .style("fill", d => { return this.colors(d.key); })
+            .attr("d", this.plot_function)
+    }
+}
---
+++ Assignment3/davbauer/main.js
@@ -0,0 +1,104 @@
+var complexChart = null
+var simpleChart = null
+var csv_data = null
+var current_data = null
+var min_year = null
+var max_year = null
+var current_year = null
+
+// This is the entry point of the app
+d3.csv('../datasets/Los_Angeles_International_Airport_-_Passenger_Traffic_By_Terminal.csv')
+  .then(csv => {
+    // log csv in browser console
+    console.log(csv)
+
+    // Set data items
+    csv_data = csv
+    current_data = csv
+
+    // Calculate year range
+    min_year = d3.min(csv, d => {
+      return new Date(d.ReportPeriod).getFullYear()
+    })
+    max_year = d3.max(csv, d => {
+      return new Date(d.ReportPeriod).getFullYear()
+    })
+
+    // Calculate mid year
+    current_year = min_year + Math.round((max_year - min_year) * 0.5)
+
+    // Set range limits
+    var range = document.getElementById("year_range")
+    range.min = min_year
+    range.max = max_year
+    range.value = current_year
+
+    // Set labels
+    d3.select("#min_year").text(min_year)
+    d3.select("#max_year").text(max_year)
+    d3.select("#current_year").text("Passengers in " + current_year)
+    d3.select("#current_flight_type").text("All Flights")
+
+    // Filter data for bar chart
+    var filtered_data = filterYear(current_data, current_year)
+
+    // Create appropriate dimensions and init the charts
+    var complexChartDimensions = {
+      width: 1200,
+      height: 500,
+      margin: {left: 150, right: 20, top: 20, bottom: 60}
+    }
+
+    var simpleChartDimensions = {
+      width: 600,
+      height: 500,
+      margin: {left: 150, right: 20, top: 20, bottom: 270}
+    }
+
+    complexChart = new ComplexChart(current_data, '#container-complex', complexChartDimensions)
+    simpleChart = new SimpleChart(filtered_data, '#container-simple', simpleChartDimensions)
+  })
+
+
+function onComplexChartSelectionChanged(value) {
+  if (complexChart == null || current_data == null)
+    return
+
+  console.log("Switching primary view to: " + value)
+  d3.select("#current_flight_type").text(value + " Flights")
+
+  if (value == "All") {
+    current_data = csv_data
+  } else {
+    current_data = filterDomesticInternational(csv_data, value == "International")
+  }
+
+  complexChart.updateChart(current_data)
+  var filtered_data = filterYear(current_data, current_year)
+  simpleChart.updateChart(filtered_data)
+}
+
+function onSimpleChartSelectionChanged(value) {
+  if (simpleChart == null || current_data == null)
+    return
+
+  console.log("Switching secondary view to year: " + value)
+
+  current_year = value
+  d3.select("#current_year").text("Passengers in " + current_year)
+
+  var filtered_data = filterYear(current_data, current_year)
+  simpleChart.updateChart(filtered_data)
+}
+
+function filterDomesticInternational(data, isInternational) {
+  return data.filter(d => {
+    return d.Domestic_International == (isInternational ? "International" : "Domestic")
+  })
+}
+
+function filterYear(data, year) {
+  return data.filter(d => {
+    return new Date(d.ReportPeriod).getFullYear() == year;
+  })
+}
