054083db1af674a1b07c7690c7f0034a9d6500d9
Suchita Mukherjee <suchita.mukherjee94@gmail.com>
2020-02-06 00:02:07 -0800

    basic data processing

--- Assignment4/sumukherjee/json_io.py
+++ Assignment4/sumukherjee/json_io.py
@@ -1,7 +1,9 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
+import json
 from flask import Flask, request, render_template
+from sklearn.cluster import KMeans
 app = Flask(__name__, template_folder="templates", static_folder="static", static_url_path="/static")
 
 @app.route("/output")
@@ -12,7 +14,17 @@ def output():
 @app.route("/output1")
 def output1():
     df = pd.read_csv('responses.csv')
-    return df.to_json()
+    #df = df.head(10)
+    df.dropna(inplace=True)
+    df.drop(df.columns.difference(['Music','Slow songs or fast songs', 'Dance, Disco, Funk', 'Folk music', 'Country', 'Classical', 'Musicals', 'Pop', 'Rock', 'Metal, Hard rock', 'Punk', 'Hip hop, Rap', 'Reggae, Ska', 'Swing, Jazz', 'Rock n Roll', 'Alternative music', 'Latin', 'Techno, Trance', 'Opera']), 1, inplace=True)
+    print(df.iloc[0])
+    #print(df.to_dict(orient = "records")[1])
+    kmeans = KMeans(n_clusters=3).fit(df)
+    centroids = kmeans.cluster_centers_
+    print(centroids)
+    print(len(kmeans.labels_))
+    print(kmeans.cluster_centers_)
+    return json.dumps(df.to_dict(orient = "records"))
 
 if __name__ == "__main__":
 	app.run()
--- Assignment4/sumukherjee/static/js/main.js
+++ Assignment4/sumukherjee/static/js/main.js
@@ -1,5 +1,42 @@
 window.onload = function() {
     $.get("output1", function(data){
-        console.log(data);
-	});
-}
+        data = JSON.parse(data);
+        var margin = {top: 10, right: 30, bottom: 30, left: 60},
+            width = 500 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var svg = d3.select("#scatter")
+            .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+              .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.scaleLinear()
+            .domain([0, 5])
+            .range([ 0, width ]);
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+        
+          // Add Y axis
+        var y = d3.scaleLinear()
+            .domain([0, 5])
+            .range([ height, 0]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+          // Add dots
+        svg.append('g')
+        .selectAll("dot")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("cx", function (d) { console.log(x(d["Music"]));console.log(y(d["Slow songs or fast songs"]));return x(d["Music"]); } )
+            .attr("cy", function (d) {  return y(d["Slow songs or fast songs"]); } )
+            .attr("r", 5)
+            .style("fill", "#69b3a2")
+      });
+    
+    }
