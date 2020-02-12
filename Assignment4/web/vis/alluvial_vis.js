b72710d5f1076c886d7bb5e3ac185182b9699cf4
Lukas Maximilian Masopust <lukasmasopust@campus-014-250.ucdavis.edu>
2020-02-11 18:12:07 -0800

    Sample code for alluvial plot

--- Assignment4/web/vis/alluvial_vis.js
+++ Assignment4/web/vis/alluvial_vis.js
@@ -17,7 +17,7 @@ class AlluvialVis {
     }
 
     transformData(data) {
-        var graph = {'nodes': [], 'links': []}
+        var graph = { 'nodes': [], 'links': [] }
 
         var n_rows = data.length
         var columns = ['sex', 'age', 'guardian', 'failures']
@@ -27,35 +27,76 @@ class AlluvialVis {
 
         var col_nodes = {}
 
-        for (const element of columns) {
-            col_nodes[element] = d3.set(data, d => d[element]).values()
-            nodes.push.apply(nodes, d3.set(data, d => d[element]).values())
-        }
-        graph.nodes = nodes
+        // for (const element of columns) {
+        //     col_nodes[element] = d3.set(data, d => d[element]).values()
+        //     var unique_values = d3.set(data, d => d[element]).values()
+
+        //     nodes.push({
+        //         node: unique_values
+        //     })
+        // }
+        // graph.nodes = nodes
 
 
-        for (var i = 0; i < columns.length - 1; i++) {
+        // for (var i = 0; i < columns.length - 1; i++) {
 
-            var items = []
-            for (const val1 of col_nodes[columns[i]]) {
-                items = data.filter(d => {
-                    return d[columns[i]] == val1
-                })
+        //     var items = []
+        //     for (const val1 of col_nodes[columns[i]]) {
+        //         items = data.filter(d => {
+        //             return d[columns[i]] == val1
+        //         })
 
-                for (const val2 of col_nodes[columns[i+1]]) {
-                    var val = items.filter(d => {
-                        return d[columns[i+1]] == val2
-                    }).length
+        //         for (const val2 of col_nodes[columns[i+1]]) {
+        //             var val = items.filter(d => {
+        //                 return d[columns[i+1]] == val2
+        //             }).length
 
-                    links.push({
-                        source: val1,
-                        target: val2,
-                        value: val / n_rows
-                    })
+        //             links.push({
+        //                 source: val1,
+        //                 target: val2,
+        //                 value: val / n_rows
+        //             })
+        //         }
+        //     }
+        // }
+        // graph.links = links
+
+        data = {
+            "nodes": [
+                {
+                    "node": 0,
+                    "name": "Africa"
+                },
+                {
+                    "node": 1,
+                    "name": "America"
+                },
+                {
+                    "node": 2,
+                    "name": "Europe"
                 }
-            }
+            ],
+            "links": [
+                {
+                    "source": 0,
+                    "target": 2,
+                    "value": 1
+                },
+                {
+                    "source": 1,
+                    "target": 2,
+                    "value": 2
+                },
+                {
+                    "source": 0,
+                    "target": 1,
+                    "value": 1
+                }
+            ]
         }
-        graph.links = links
+
+        graph.links = data.links
+        graph.nodes = data.nodes
 
         return graph
     }
@@ -65,7 +106,7 @@ class AlluvialVis {
         d3.select(this.html_root + " > *").remove()
 
         var svg = d3.select(this.html_root)
-        .append('svg')
+            .append('svg')
             .attr('width', this.width + this.margin.left + this.margin.right)
             .attr('height', this.height + this.margin.top + this.margin.bottom)
 
@@ -79,11 +120,56 @@ class AlluvialVis {
 
         var path = sankey.link()
 
-        //sankey
-        //    .nodes(this.graph.nodes)
-        //    .links(this.graph.links)
-        //    .layout(32)
+        sankey
+            .nodes(this.graph.nodes)
+            .links(this.graph.links)
+            .layout(32)
 
-        
+        // add in the links
+        var link = svg.append("g").selectAll(".link")
+            .data(this.graph.links)
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", path)
+            .style("stroke-width", function (d) { return Math.max(1, d.dy); })
+            .sort(function (a, b) { return b.dy - a.dy; });
+
+        // // add the link titles
+        // link.append("title")
+        //     .text(function (d) {
+        //         return d.source.name + " → " +
+        //             d.target.name + "\n" + format(d.value);
+        //     });
+
+        // add in the nodes
+        var node = svg.append("g").selectAll(".node")
+            .data(this.graph.nodes)
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            })
+            // .call(d3.behavior.drag()
+            //     .origin(function (d) { return d; })
+            //     .on("dragstart", function () {
+            //         this.parentNode.appendChild(this);
+            //     })
+            //     .on("drag", dragmove));
+
+        // add the rectangles for the nodes
+        node.append("rect")
+            .attr("height", function (d) { return d.dy; })
+            .attr("width", sankey.nodeWidth())
+            .style("fill", function (d) {
+                return d.color = color(d.name.replace(/ .*/, ""));
+            })
+            .style("stroke", function (d) {
+                return d3.rgb(d.color).darker(2);
+            })
+            .append("title")
+            .text(function (d) {
+                return d.name + "\n" + format(d.value);
+            });
+
     }
 }
