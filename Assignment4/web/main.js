a793ce3a56bce41423037dd37988057f98b2e25f
YunHsin Kuo <yhkuo@campus-001-022.ucdavis.edu>
2020-02-13 13:06:12 -0800

    histogram stuff

--- Assignment4/web/main.js
+++ Assignment4/web/main.js
@@ -1,5 +1,12 @@
 var data = null
-var example_vis = null
+var hist_choices = ["G1", "G2", "G3", "age", "absences"]
+var key = hist_choices[0]
+var setting = {
+  key: key,
+  x_domain: {min: 0, max: 22},
+  x_ticks: 80,
+  x_axis: key.charAt(0).toUpperCase() + key.slice(1)
+}
 
 // This is the entry point of the app
 d3.csv('../dataset/student-mat.csv')
@@ -8,14 +15,43 @@ d3.csv('../dataset/student-mat.csv')
     console.log(csv)
 
     // Set data items
-    data = csv
+    data = csv //maybe we could use the data selected in alluvial chart?
+    highlight_data = csv.slice(0,100); //here should be fed with selected data from scatter plot.
 
     // Create appropriate dimensions and init the charts
-    var example_vis_dimensions = {
+    var hist_vis_dimensions = {
       width: 600,
-      height: 500,
-      margin: {left: 150, right: 20, top: 20, bottom: 270}
+      height: 400,
+      margin: {left: 100, right: 20, top: 20, bottom: 150}
     }
 
-    example_vis = new ExampleVis(data, '#example-vis-container', example_vis_dimensions)
+    //change to the input as hist_vis
+    hist_vis = new Histogram(data, highlight_data,'#hist-vis', hist_vis_dimensions, setting);
   })
+
+function ThirdDropdownChange(value){
+  var history = setting.key
+  setting.key = value
+  setting.x_axis = value.charAt(0).toUpperCase() + value.slice(1)
+  //If we are gonna update a histogram
+  if(hist_choices.includes(value)){
+    //Adjust domain
+    setting.x_domain.min = 0
+    setting.x_domain.max = 22
+    if (value == "age") {
+      setting.x_domain.min = 10
+      setting.x_domain.max = 25
+    } else if (value == "absences"){
+      setting.x_domain.max = 80
+    }
+
+    hist_vis.update(data, highlight_data, setting, true)
+
+  } else {
+  //else, we are gonna update a barchart
+
+    hist_vis.update(data, highlight_data, setting, false)
+
+  }
+
+}
---
+++ Assignment4/web/vis/hist.js
@@ -0,0 +1,293 @@
+class Histogram {
+    constructor(data, h_data, html_root, dimensions, setting){
+        this.setting = setting
+        this.data = this.transformData_Hist(data, setting.key)
+        this.h_data = this.transformData_Hist(h_data, setting.key)
+        this.html_root = html_root
+
+        this.width = dimensions.width
+        this.height = dimensions.height
+        this.margin = dimensions.margin
+
+        this.x = null
+        this.y = null
+        this.xAxis = null
+        this.yAxis = null
+        this.plot_function = null
+
+        this.mouseover = null
+        this.mousemove = null
+        this.mouseleave = null
+        //somehow just cannot make tooltip here.
+
+        this.init()
+    }
+
+    transformData_Hist(data, key) {
+      var hist = d3.histogram()
+        .domain([this.setting.x_domain.min, this.setting.x_domain.max])
+        .thresholds([...Array(this.setting.x_ticks+1).keys()]);
+
+      var temp = hist(data.map(d => d[key]).map(d => { return +d }) ).map(d => {
+        return{
+          x0: d.x0,
+          x1: d.x1,
+          value: d.length
+        }
+      })
+
+      return temp
+    }
+
+    transformData_Bar(data, key){
+      var nested = d3.nest()
+      .key(d => d[key])
+      .rollup(v => v.length)
+      .entries(data)
+
+      return nested.map(d => {
+          return {
+              key: d.key,
+              value: +d.value,
+          }
+      }).sort((a, b) => d3.ascending(a.key, b.key))
+    }
+
+    init(){
+        // Clear the tag
+        d3.select(this.html_root + " > *").remove()
+
+        // Add new items
+        var svg = d3.select(this.html_root)
+        .append('svg')
+            .attr('width', this.width + this.margin.left + this.margin.right)
+            .attr('height', this.height + this.margin.top + this.margin.bottom)
+
+        var view = svg.append("g")
+            .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")")
+
+        var tooltip = d3.select("#hist-vis")
+            .append("div")
+              .style("opacity", 0)
+              .attr("class", "tooltip")
+              .style("background-color", "black")
+              .style("border-radius", "2px")
+              .style("padding", "5px")
+              .style("width", "100px")
+              .style("height", "15px")
+              .attr("font-anchor", "mid")
+
+        this.mouseover = function(d) {
+          tooltip.html("<span style='color: white'>" + d.value + " students </span>")
+            .style("left", (d3.mouse(this)[0]+10) + "px")
+            .style("top", (d3.mouse(this)[1]) + "px")
+
+          d3.select(this)
+            .style("stroke", "gray")
+
+          tooltip
+            .transition()
+            .duration(200)
+              .style("opacity", 0.6)
+        }
+
+        this.mousemove = function(d){
+          tooltip
+            .style("left", (d3.mouse(this)[0]+10) + "px")
+            .style("top", (d3.mouse(this)[1]) + "px")
+        }
+
+        this.mouseleave = function(d){
+          tooltip
+            .transition()
+            .duration(200)
+              .style("opacity", 0)
+
+          d3.select(this)
+            .style("stroke", "none")
+        }
+
+        //scale functions
+        this.x = d3.scaleLinear()
+          .domain([this.setting.x_domain.min, this.setting.x_domain.max])
+          .range([0,this.width]);
+
+        this.y = d3.scaleLinear()
+          .domain([0, d3.max(this.data, function(d){ return d.value; })])
+          .range([this.height, 0]);
+
+        //axis
+        this.xAxis = d3.axisBottom(this.x)
+        this.yAxis = d3.axisLeft(this.y)
+
+        //create the bar rectangles for histogram
+        view.selectAll(".base")
+          .data(this.data)
+          .enter()
+          .append("rect")
+            .attr("class", "base")
+            .attr("fill", "lightgray")
+            .style("fill-opacity", 0.6)
+            .attr("x", d => { return this.x(d.x0) })
+            .attr("y", d => { return this.y(d.value) })
+            .attr("width", d => { return this.x(d.x1) - this.x(d.x0) })
+            .attr("height", d => { return this.height - this.y(d.value) })
+          .on("mouseover", this.mouseover)
+          .on("mousemove", this.mousemove)
+          .on("mouseleave", this.mouseleave)
+
+
+        view.selectAll(".highlight")
+          .data(this.h_data)
+          .enter()
+          .append("rect")
+            .attr("class", "highlight")
+            .attr("fill", "#69b3a2")
+            .style("fill-opacity", 0.6)
+            .attr("x", d => { return this.x(d.x0) })
+            .attr("y", d => { return this.y(d.value) })
+            .attr("width", d => { return this.x(d.x1) - this.x(d.x0) })
+            .attr("height", d => { return this.height - this.y(d.value) })
+          .on("mouseover", this.mouseover)
+          .on("mousemove", this.mousemove)
+          .on("mouseleave", this.mouseleave)
+
+        //x axis
+        view.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + this.height + ")")
+          .call(this.xAxis)
+          .append("text")
+            .attr("id", "x_title")
+            .attr("fill", "#000")
+            .attr("x", this.width / 2 )
+            .attr("y", 60)
+            .attr("text-anchor", "mid")
+            .text(this.setting.x_axis)
+
+        //y axis
+        view.append("g")
+          .attr("class", "y axis")
+          .call(this.yAxis)
+          .append("text")
+            .attr("fill", "#000")
+            .attr("transform", "rotate(-90)")
+            .attr("x", -this.height / 2)
+            .attr("y", -60)
+            .attr("text-anchor", "mid")
+            .text("# of Students")
+
+
+    }
+
+    update(data, h_data, setting, isHist){
+      console.log("start update: ", setting.x_axis)
+      this.setting = setting
+
+      //re-transform the data
+      if (isHist) this.retransform_Hist(data, h_data)
+      else this.retransform_Bar(data, h_data)
+
+      d3.select(this.html_root).select(".x.axis")
+        .transition()
+        .duration(1000)
+        .call(this.xAxis)
+        .select("#x_title")
+          .text(this.setting.x_axis)
+        .selectAll("text")
+
+
+      d3.select(this.html_root).select(".y.axis")
+        .transition()
+        .duration(500)
+        .call(this.yAxis)
+
+      // update the rectangles
+      var base_bars = d3.select(this.html_root).select("g").selectAll(".base").data(this.data)
+      var highlight_bars = d3.select(this.html_root).select("g").selectAll(".highlight").data(this.h_data)
+
+      var tooltip = d3.select(".vis-container")
+          .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "black")
+            .style("border-radius", "2px")
+            .style("padding", "5px")
+            .style("width", "100px")
+            .style("height", "15px")
+            .attr("font-anchor", "mid")
+
+      if(isHist){
+        this.updateBars_Hist(base_bars, "lightgray", "base")
+        this.updateBars_Hist(highlight_bars, "#69b3a2", "highlight")
+      } else {
+        this.updateBars_Bar(base_bars, "lightgray", "base")
+        this.updateBars_Bar(highlight_bars, "#69b3a2", "highlight")
+      }
+
+      d3.selectAll(".base, .highlight")
+        .on("mouseover", this.mouseover)
+        .on("mousemove", this.mousemove)
+        .on("mouseleave", this.mouseleave)
+    }
+
+    retransform_Hist(data, h_data){
+      this.data = this.transformData_Hist(data, this.setting.key)
+      this.h_data = this.transformData_Hist(h_data, this.setting.key)
+
+      this.x = d3.scaleLinear()
+        .domain([this.setting.x_domain.min, this.setting.x_domain.max])
+        .range([0,this.width]);
+      this.xAxis = d3.axisBottom(this.x)
+      this.y.domain([0, d3.max(this.data, d => d.value)])
+    }
+
+    retransform_Bar(data, h_data){
+      this.data = this.transformData_Bar(data, this.setting.key)
+      this.h_data = this.transformData_Bar(h_data, this.setting.key)
+
+      this.x = d3.scaleBand()
+          .domain(this.data.map(d =>  d.key))
+          .range([0, this.width])
+          .padding(0.2)
+      this.xAxis = d3.axisBottom(this.x)
+      this.y.domain([0, d3.max(this.data, d => d.value)])
+    }
+
+    updateBars_Hist(bars, color, bar_class){
+      bars.enter()
+        .append("rect")
+        .attr("class", bar_class)
+        .merge(bars)
+          .transition()
+          .duration(500)
+            .attr("fill", color)
+            .style("fill-opacity", 0.6)
+            .attr("x", d => { return this.x(d.x0) })
+            .attr("y", d => { return this.y(d.value)} )
+            .attr("width", d => { return this.x(d.x1) - this.x(d.x0) })
+            .attr("height", d => { return this.height - this.y(d.value)})
+
+      bars.exit().remove()
+    }
+
+    updateBars_Bar(bars, color, bar_class){
+      bars.enter()
+        .append("rect")
+        .attr("class", bar_class)
+        .merge(bars)
+          .transition()
+          .duration(500)
+            .attr("fill", color)
+            .style("fill-opacity", 0.6)
+            .attr("x", d => { return this.x(d.key) })
+            .attr("y", d => { return this.y(d.value) })
+            .attr("width", this.x.bandwidth())
+            .attr("height", d => { return this.height - this.y(d.value) })
+
+      bars.exit().remove()
+
+    }
+
+
+}
