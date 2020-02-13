5da3d2aedcd0ef6df849496482dba05be3e50acd
jacksonvanover <jacksonvanover@gmail.com>
2020-02-12 16:21:16 -0800

    rough draft

--- Assignment4/app.py
+++ Assignment4/app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
@@ -42,14 +43,23 @@ def dummy_cluster_norm(data, numCluster):
 CURRENT_CLUSTER_NO = 3
 
 try:
+
+    # load dataFrame of preprocessed data
     with open("__temp", 'rb') as f:
         df = pickle.load(f)
+
 except:
-    # load responses as a DataFrame
+
+    # load responses as a DataFrame from raw CSV
     with open("responses.csv") as f:
         df = pd.read_csv(f)
+    
+    # fill in missing values with the average of each column and
+    # perform initial clustering
     df.fillna(df.mean(), inplace=True)
     df = dummy_cluster_norm(df, 3)
+    
+    # save preprocessed data
     with open("__temp", "wb") as f:
         pickle.dump(df, f)
 
@@ -70,70 +80,121 @@ question_categories = {
     "Demographics" : list(df.columns[140:150])
 }
 
+
+default_colors = [
+    '#1f77b4',  # muted blue
+    '#ff7f0e',  # safety orange
+    '#d62728',  # brick red
+    '#8c564b',  # chestnut brown
+    '#e377c2',  # raspberry yogurt pink
+    '#7f7f7f',  # middle gray
+    '#bcbd22',  # curry yellow-green
+    '#17becf'   # blue-teal
+    '#2ca02c',  # cooked asparagus green
+]
+
 def render_visualization():
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
     app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
     app.layout = html.Div([
-        html.Div(html.H1("Title")),
-    
-        html.Div(html.H4("Subtitle")),
 
         html.Div([
-                
-            dcc.Graph(id='3d-plot', style={'width': '50%'}),
+            html.Div([
 
-            dcc.Dropdown(
-                id = 'cluster-dropdown',
-                options=[
-                    {'label' : '2 Clusters', 'value' : 2},
-                    {'label' : '3 Clusters', 'value' : 3},
-                    {'label' : '4 Clusters', 'value' : 4},
-                    {'label' : '5 Clusters', 'value' : 5},
-                    {'label' : '6 Clusters', 'value' : 6},
-                    {'label' : '7 Clusters', 'value' : 7}],
-                value=3,
-                style={'width': '53%'}
-            )
-        ]),
+                dcc.Checklist(
+                    id = 'question-dropdown',
+                    options=[
+                        {'label' : 'Music', 'value' : 'Music'},
+                        {'label' : 'Movies', 'value' : 'Movies'},
+                        {'label' : 'Hobbies', 'value' : 'Hobbies'},
+                        {'label' : 'Phobias', 'value' : 'Phobias'},
+                        {'label' : 'Health', 'value' : 'Health'},
+                        {'label' : 'Personality', 'value' : 'Personality'},
+                        {'label' : 'Spending', 'value' : 'Spending'},
+                        {'label' : 'Demographics', 'value' : 'Demographics'}],
+                    value=['Music','Movies'],
+                    className='six columns',
+                    style={'width': '45%'}
+                ),
+
+                dcc.RadioItems(
+                    id = 'cluster-dropdown',
+                    options=[
+                        {'label' : '2 Clusters', 'value' : 2},
+                        {'label' : '3 Clusters', 'value' : 3},
+                        {'label' : '4 Clusters', 'value' : 4},
+                        {'label' : '5 Clusters', 'value' : 5},
+                        {'label' : '6 Clusters', 'value' : 6},
+                        {'label' : '7 Clusters', 'value' : 7}],
+                    value=3,
+                    className='six columns',
+                    style={'width': '45%'}
+                )
+            ], className="three columns", style={'backgroundColor' : 'rgb(220,220,220)', 'width' : '25%'}),
+            
+            html.Div([
+                html.Div(html.H1("Young People Survey")),
+            
+                html.Div(html.H4("Subtitle")),
+            ],className="three columns", style={'width' : '60%'})
+        ],className="row"),
 
         html.Div([
-                
-            dcc.Graph(id='corr-heatmap', style={'width': '100%'}),
-
-            dcc.Dropdown(
-                id = 'question-dropdown',
-                options=[
-                    {'label' : 'Music', 'value' : 'Music'},
-                    {'label' : 'Movies', 'value' : 'Movies'},
-                    {'label' : 'Hobbies', 'value' : 'Hobbies'},
-                    {'label' : 'Phobias', 'value' : 'Phobias'},
-                    {'label' : 'Health', 'value' : 'Health'},
-                    {'label' : 'Personality', 'value' : 'Personality'},
-                    {'label' : 'Spending', 'value' : 'Spending'},
-                    {'label' : 'Demographics', 'value' : 'Demographics'}],
-                value=['Music','Movies'],
-                multi=True,
-                style={'width': '53%'}
-            )
-        ]),
-
-        html.Div(dcc.Graph(id='violins',style={'width':'100%'}),style={'maxWidth' : '1000px', 'overflowX' : 'scroll'})
+            html.Div(
+                    dcc.Graph(
+                            id='3d-plot',
+                            style={'height' : '900px'}
+                        ),
+                    className='six columns',
+                    style={'width': '48%', 'height': '1000px'}
+                ),
+            html.Div(
+                    dcc.Graph(
+                        id='corr-heatmap',
+                        style={'height' : '900px'}
+                    ),
+                    className='six columns',
+                    style={'width': '48%', 'height' : '1000px'}),
+        ], className="row", style={}),
+        
+        html.Div(dcc.Graph(id='violins',style={'width':'100%'}),
+                style={'maxWidth' : '100%', 'overflowX' : 'scroll'},
+                className='row')
 
     ])
 
-    @app.callback(
-        Output('3d-plot', 'figure'),
-        [Input('cluster-dropdown', 'value')]
-    )
-    def update_figure(clusterNo):
-        global df
+    # # updates the 3d scatterplot of survey response clusters
+    # @app.callback(
+    #     Output('3d-plot', 'figure'),
+    #     [Input('cluster-dropdown', 'value')]
+    # )
+    # def update_figure(clusterNo):
 
-        if clusterNo != CURRENT_CLUSTER_NO:
-            df = dummy_cluster_norm(df, clusterNo)
+    #     global df
+    #     global CURRENT_CLUSTER_NO
 
-        return px.scatter_3d(df, x='xcoord', y='ycoord', z='zcoord', color='clusterNo')
+    #     if clusterNo != CURRENT_CLUSTER_NO:
+    #         df = dummy_cluster_norm(df, clusterNo)
 
+    #     fig = go.Figure()
+
+    #     for x in range(clusterNo):
+    #         fig.add_trace(go.Scatter3d(x=df[df.clusterNo == str(x)]['xcoord'].tolist(),
+    #                                     y=df[df.clusterNo == str(x)]['ycoord'].tolist(),
+    #                                     z=df[df.clusterNo == str(x)]['zcoord'].tolist(),
+    #                                     mode='markers',
+    #                                     marker={'color' : default_colors[x]},
+    #                                     name="Cluster {}".format(x)
+    #                                 )
+    #         )
+
+    #     fig.update_layout(showlegend=True, legend={'x' : 0.8, 'y' : 0.9, 'bordercolor': "Black", 'borderwidth': 1})
+
+    #     return  fig#px.scatter_3d(df, x='xcoord', y='ycoord', z='zcoord', color='clusterNo')
+
+
+    # updates the heatmap of correlation coefficients
     @app.callback(
         Output('corr-heatmap', 'figure'),
         [Input('question-dropdown', 'value'),
@@ -141,7 +202,7 @@ def render_visualization():
     )
     def update_heatmap(category_choices, click_data):
         if click_data:
-            chosen_cluster = str(click_data["points"][0]['marker.color'])
+            chosen_cluster = str(click_data["points"][0]['curveNumber'])
         else:
             chosen_cluster = "0"
         
@@ -153,36 +214,84 @@ def render_visualization():
 
         df_selected = df_selected[chosen_columns]
         
-        return go.Figure(data=go.Heatmap(z=df_selected.corr().values.tolist(), x=df_selected.columns, y=df_selected.columns, colorscale='gray'))
+        matrix = df_selected.corr().values.tolist()
+        mask = np.tri(len(df_selected.columns), k=0)
+        matrix = np.ma.array(matrix, mask=mask)
 
+        fig = go.Figure(data=go.Heatmap(z=[np.flip(x) for x in matrix.filled(np.nan)],
+                                        x=[x for x in np.flip(df_selected.columns)[:-1]],
+                                        y=[x for x in df_selected.columns[:-1]],
+                                        colorscale='gray_r',
+                                        hoverongaps=False
+                                        )
+                        )
+
+        fig.update_layout(title={'text' : "Correlation Coefficients in Cluster {}".format(chosen_cluster),
+                                    'x' : 0.34,
+                                    'xanchor' : 'left',
+                                    'y' : 0.85,
+                                    'yanchor' : 'middle'
+                                },
+                            titlefont={'size' : 30},
+                            xaxis={'showgrid' : False},
+                            yaxis={'showgrid' : False},
+                            plot_bgcolor='rgb(255,255,255)'
+                        )
+        
+        return fig
+
+
+    # updates the violin plot of survey responses
     @app.callback(
-        Output('violins', 'figure'),
-        [Input('question-dropdown', 'value')]
+        [Output('violins', 'figure'),
+        Output('3d-plot', 'figure')],
+        [Input('question-dropdown', 'value'),
+        Input('cluster-dropdown', 'value')]
     )
-    def update_violins(category_choices):
+    def update_figures(category_choices, number_of_clusters):
+
+        global df
+        global CURRENT_CLUSTER_NO
+
+        if number_of_clusters != CURRENT_CLUSTER_NO:
+            df = dummy_cluster_norm(df, number_of_clusters)
+
+        scatter_plot = go.Figure()
+
+        for x in range(number_of_clusters):
+            scatter_plot.add_trace(go.Scatter3d(x=df[df.clusterNo == str(x)]['xcoord'].tolist(),
+                                        y=df[df.clusterNo == str(x)]['ycoord'].tolist(),
+                                        z=df[df.clusterNo == str(x)]['zcoord'].tolist(),
+                                        mode='markers',
+                                        marker={'color' : default_colors[x]},
+                                        name="Cluster {}".format(x)
+                                    )
+            )
+
+        scatter_plot.update_layout(showlegend=True, legend={'x' : 0.8, 'y' : 0.9, 'bordercolor': "Black", 'borderwidth': 1})
 
         chosen_columns = []
         for choice in category_choices:
             chosen_columns = chosen_columns + question_categories[choice]
 
-        fig = go.Figure()
+        violins = go.Figure()
 
         for column in chosen_columns:
-            for clusterNo in range(3):
-                fig.add_trace(go.Violin(y=df[column][df['clusterNo'] == str(clusterNo)],
+            for clusterNo in range(number_of_clusters):
+                violins.add_trace(go.Violin(y=df[column][df['clusterNo'] == str(clusterNo)],
                                         name=headerDict[column],
                                         scalegroup=headerDict[column],
                                         legendgroup=clusterNo,
                                         #box_visible=True,
                                         meanline_visible=True,
-                                        line_color='rgb({},0,0)'.format(255/(clusterNo+1)),
-                                        opacity=0.3)
+                                        line_color=default_colors[clusterNo],
+                                        opacity=0.4)
                             )
 
-        fig.update_traces(side='positive', points=False, width = 1.5)
-        fig.update_layout(xaxis_showgrid=False, xaxis_zeroline=False, width=len(chosen_columns) * 100, autosize=False)
+        violins.update_traces(side='positive', points=False, width = 1.5)
+        violins.update_layout(xaxis_showgrid=False, xaxis_zeroline=False, width=len(chosen_columns) * 100, autosize=False)
 
-        return fig
+        return violins, scatter_plot
 
     app.run_server(debug=True)
 
