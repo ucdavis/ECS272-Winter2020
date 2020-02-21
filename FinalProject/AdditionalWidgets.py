30b57f733741d765f1352745ddea217caf27d9c2
MLivanos <prodigiousmike@gmail.com>
2020-02-20 21:07:20 -0800

    Add: Masking model, start of frontend UI

---
+++ FinalProject/AdditionalWidgets.py
@@ -0,0 +1,137 @@
+'''Contains custom QWidgets
+Copyright Renkert Oil LLC 2019. All rights reserved'''
+
+from PyQt5 import QtWidgets, QtCore, QtGui
+
+'''QWidget that brings up file explorer and returns string of file location'''
+class FileEntry(QtWidgets.QWidget):
+    '''Creates a entry box and an associated browse button'''
+    def __init__(self, parent=None, t='file'):
+        
+        QtWidgets.QWidget.__init__(self, parent)
+        
+        self.type = t #Type is 'file' or 'folder' (what you want to open)
+        
+        self.grid = QtWidgets.QGridLayout()
+        
+        self.entry_box = QtWidgets.QLineEdit(self)
+        self.grid.addWidget(self.entry_box, 0, 0)
+        
+        self.browse_button = QtWidgets.QPushButton('Browse', self)
+        self.browse_button.clicked.connect(self.browse_files)
+        self.grid.addWidget(self.browse_button, 0, 1)
+        
+        self.textChanged = self.entry_box.textChanged
+        
+        self.setLayout(self.grid)
+        self.show()
+    
+    def browse_files(self):
+        if self.type == 'file':
+            '''NOTE: Name is coming in as a tuple for some reason. 
+            Maybe part of the Qt5 change? in any case, this should
+            be investiaged to ensure that it will always be a tuple'''
+            name = QtWidgets.QFileDialog.getOpenFileName(self, 'Browse Files') 
+        else:
+            name = QtWidgets.QFileDialog.getExistingDirectory(self,
+                'Browse Folders')
+            
+        if name != '': #The dialog box wasn't canceled
+            self.entry_box.setText(name[0])
+    
+    def setText(self, text):
+        self.entry_box.setText(text)
+
+    def text(self):
+        '''returns the entry_box text'''
+        return self.entry_box.text()
+
+class RangeSlider(QtWidgets.QWidget):
+
+    def __init__(self, maxval=100):
+        super().__init__()
+
+        self.minTime = 0
+        self.maxTime = 0
+        self.minRangeTime = 0
+        self.maxRangeTime = 0
+
+        self.sliderMin = maxval
+        self.sliderMax = maxval
+
+        self.setupUi(self)
+
+    def setupUi(self, RangeSlider):
+        RangeSlider.setObjectName("RangeSlider")
+        RangeSlider.resize(1000, 65)
+        RangeSlider.setMaximumSize(QtCore.QSize(16777215, 65))
+        self.RangeBarVLayout = QtWidgets.QVBoxLayout(RangeSlider)
+        self.RangeBarVLayout.setContentsMargins(5, 0, 5, 0)
+        self.RangeBarVLayout.setSpacing(0)
+        self.RangeBarVLayout.setObjectName("RangeBarVLayout")
+
+        self.slidersFrame = QtWidgets.QFrame(RangeSlider)
+        self.slidersFrame.setMaximumSize(QtCore.QSize(16777215, 25))
+        self.slidersFrame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.slidersFrame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.slidersFrame.setObjectName("slidersFrame")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.slidersFrame)
+        self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetMinimumSize)
+        self.horizontalLayout.setContentsMargins(5, 2, 5, 2)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        ## Start Slider Widget
+        self.startSlider = QtWidgets.QSlider(self.slidersFrame)
+        self.startSlider.setMaximum(self.sliderMin)
+        self.startSlider.setMinimumSize(QtCore.QSize(100, 5))
+        self.startSlider.setMaximumSize(QtCore.QSize(16777215, 10))
+
+        font = QtGui.QFont()
+        font.setKerning(True)
+
+        self.startSlider.setFont(font)
+        self.startSlider.setAcceptDrops(False)
+        self.startSlider.setAutoFillBackground(False)
+        self.startSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.startSlider.setInvertedAppearance(True)
+        self.startSlider.setObjectName("startSlider")
+        self.startSlider.setValue(self.sliderMax)
+        self.startSlider.valueChanged.connect(self.handleStartSliderValueChange)
+        self.horizontalLayout.addWidget(self.startSlider)
+
+        ## End Slider Widget
+        self.endSlider = QtWidgets.QSlider(self.slidersFrame)
+        self.endSlider.setMaximum(self.sliderMax)
+        self.endSlider.setMinimumSize(QtCore.QSize(100, 5))
+        self.endSlider.setMaximumSize(QtCore.QSize(16777215, 10))
+        self.endSlider.setTracking(True)
+        self.endSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.endSlider.setObjectName("endSlider")
+        self.endSlider.setValue(self.sliderMax)
+        self.endSlider.valueChanged.connect(self.handleEndSliderValueChange)
+
+        #self.endSlider.sliderReleased.connect(self.handleEndSliderValueChange)
+
+        self.horizontalLayout.addWidget(self.endSlider)
+
+        self.RangeBarVLayout.addWidget(self.slidersFrame)
+
+        #self.retranslateUi(RangeSlider)
+        QtCore.QMetaObject.connectSlotsByName(RangeSlider)
+
+        self.show()
+
+    @QtCore.pyqtSlot(int)
+    def handleStartSliderValueChange(self, value):
+        self.startSlider.setValue(value)
+
+    @QtCore.pyqtSlot(int)
+    def handleEndSliderValueChange(self, value):
+        self.endSlider.setValue(value)
+
+
+
+
+
+
---
+++ FinalProject/Frontend.py
@@ -0,0 +1,138 @@
+import sys
+import webbrowser
+import os
+import time
+import random
+import string
+from PyQt5 import QtCore, QtWidgets, QtGui
+from AdditionalWidgets import FileEntry, RangeSlider
+from shutil import copyfile
+
+'''Primary source of interaction between all kinds of users
+and computer.'''
+class MainWindow(QtWidgets.QMainWindow):
+
+	def __init__(self, parent=None):
+		QtWidgets.QMainWindow.__init__(self, parent)
+
+		self.parent = parent
+		self.cwd = os.getcwd()
+		# this is the widget we will populate with the main menu widgets
+		self.cw = QtWidgets.QWidget(self)
+		self.setCentralWidget(self.cw)
+
+		self.showMaximized()
+
+		self.grid = QtWidgets.QGridLayout()
+
+		self.createTabs()
+		self.createSetupWindow()
+		self.createViewWindow()
+
+		self.cw.setLayout(self.grid)
+
+		screenShape = QtWidgets.QDesktopWidget().screenGeometry(self)
+		self.width = 3.0 * screenShape.width() / 9.0
+		self.height = 3.0 * screenShape.height() / 4.0
+		self.resize(self.width, self.height)
+
+	def createTabs(self):
+		self.tab_widget = QtWidgets.QTabWidget()
+		# Create tabs
+		self.setup_tab = QtWidgets.QWidget()
+		self.view_tab = QtWidgets.QWidget()
+		self.analysis_tab = QtWidgets.QWidget()
+		# Create individual tab layouts
+		self.setup_tab.layout = QtWidgets.QVBoxLayout(self)
+		self.view_tab.layout = QtWidgets.QVBoxLayout(self)
+		self.analysis_tab.layout = QtWidgets.QVBoxLayout(self)
+		# Add tabs to tab widget
+		self.tab_widget.addTab(self.setup_tab, 'Setup')
+		self.tab_widget.addTab(self.view_tab, 'View')
+		self.tab_widget.addTab(self.analysis_tab, 'Analysis')
+		# Add tab widget to MW
+		self.grid.addWidget(self.tab_widget, 0, 0)
+
+	def createSetupWindow(self):
+		# Setup window grid and frame
+		self.setup_grid = QtWidgets.QGridLayout()
+		self.setup_frame = QtWidgets.QFrame(self.cw)
+		self.setup_frame.setFrameShadow(QtWidgets.QFrame.Sunken)
+		self.setup_frame.setFrameShape(QtWidgets.QFrame.Panel)
+		# Setup widgets
+		self.setup_title = QtWidgets.QLabel("Setup")
+		self.setup_title.setFont(QtGui.QFont("Arial", 24, QtGui.QFont.Bold))
+		self.path_entry = FileEntry()
+		self.raw_image = QtWidgets.QLabel()
+		raw_pixmap = QtGui.QPixmap(os.path.join('images', 'placeholder.jpg'))
+		self.raw_image.setPixmap(raw_pixmap.scaled(480, 360))
+		self.advanced_label = QtWidgets.QLabel("Advanced")
+		self.advanced_label.setFont(QtGui.QFont("Arial", 16, QtGui.QFont.Bold))
+		self.prediction_confidence_label = QtWidgets.QLabel("Prediction Confidence")
+		self.masking_confidence_label = QtWidgets.QLabel("Masking Confidence")
+		self.prediction_confidence_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+		self.masking_confidence_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+		# Slider values
+		self.prediction_confidence_slider.setMinimum(0)
+		self.prediction_confidence_slider.setMaximum(100)
+		self.prediction_confidence_slider.setValue(50)
+		self.masking_confidence_slider.setMinimum(0)
+		self.masking_confidence_slider.setMaximum(100)
+		self.masking_confidence_slider.setValue(30)
+		# Add setup widgets to setup grid
+		self.setup_grid.addWidget(self.setup_title, 0, 0)
+		self.setup_grid.addWidget(self.path_entry, 1, 0)
+		self.setup_grid.addWidget(self.raw_image, 1, 1)
+		self.setup_grid.addWidget(self.advanced_label, 2, 0)
+		self.setup_grid.addWidget(self.prediction_confidence_label, 3, 0)
+		self.setup_grid.addWidget(self.prediction_confidence_slider, 4, 0, 1, 2)
+		self.setup_grid.addWidget(self.masking_confidence_label, 5, 0)
+		self.setup_grid.addWidget(self.masking_confidence_slider, 6, 0, 1, 2)
+		# Set setup layout
+		self.setup_tab.setLayout(self.setup_grid)
+
+	def createViewWindow(self):
+		# Setup window grid and frame
+		self.view_grid = QtWidgets.QGridLayout()
+		self.view_frame = QtWidgets.QFrame(self.cw)
+		self.view_frame.setFrameShadow(QtWidgets.QFrame.Sunken)
+		self.view_frame.setFrameShape(QtWidgets.QFrame.Panel)
+		# Setup widgets
+		self.view_title = QtWidgets.QLabel("View")
+		self.view_title.setFont(QtGui.QFont("Arial", 24, QtGui.QFont.Bold))
+		self.view_image = QtWidgets.QLabel()
+		view_pixmap = QtGui.QPixmap(os.path.join('images', 'placeholder.jpg'))
+		self.view_image.setPixmap(view_pixmap.scaled(480, 360))
+		self.value_lowend_label = QtWidgets.QLabel("$")
+		self.price_range_slider = RangeSlider()
+		self.value_highend_label = QtWidgets.QLabel("$$$")
+		self.view_changer = QtWidgets.QButtonGroup()
+		self.category_button = QtWidgets.QRadioButton("Category")
+		self.value_button = QtWidgets.QRadioButton("Value")
+		self.weight_button = QtWidgets.QRadioButton("Weight")
+		self.view_changer.addButton(self.category_button)
+		self.view_changer.addButton(self.value_button)
+		self.view_changer.addButton(self.weight_button)
+		self.category_tree = QtWidgets.QTreeWidget()
+		# Add setup widgets to setup grid
+		self.view_grid.addWidget(self.view_title, 0, 0)
+		self.view_grid.addWidget(self.view_image, 1, 0, 4, 3)
+		self.view_grid.addWidget(self.value_lowend_label, 5, 0)
+		self.view_grid.addWidget(self.price_range_slider, 5, 1)
+		self.view_grid.addWidget(self.value_highend_label, 5, 2)
+		self.view_grid.addWidget(self.category_button, 1, 3)
+		self.view_grid.addWidget(self.value_button, 2, 3)
+		self.view_grid.addWidget(self.weight_button, 3, 3)
+		self.view_grid.addWidget(self.category_tree, 4, 3)
+		# Set setup layout
+		self.view_tab.setLayout(self.view_grid)
+
+'''Launches MainWindow object'''
+def launch(filename=None):
+	app = QtWidgets.QApplication(sys.argv)
+	mw = MainWindow()
+	sys.exit(app.exec_())
+
+
+'''Pilot'''
+launch()
