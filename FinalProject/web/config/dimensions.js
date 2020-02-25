4c7a771e7d72daabaf8fd826370afb2b787aa0fc
Lukas Maximilian Masopust <lmasopust@ucdavis.edu>
2020-02-25 11:10:28 -0800

    Added map_vis skeleton

--- FinalProject/web/config/dimensions.js
+++ FinalProject/web/config/dimensions.js
@@ -5,3 +5,9 @@ var example_vis_dimensions = {
     height: 500,
     margin: { left: 150, right: 20, top: 20, bottom: 50 }
 }
+
+var map_vis_dimensions = {
+    width: 600,
+    height: 500,
+    margin: { left: 150, right: 20, top: 20, bottom: 50 }
+}
---
+++ FinalProject/web/vis/map_vis.js
@@ -0,0 +1,23 @@
+class MapVis {
+    constructor(data, html_root, dimensions) {
+        this.data = data
+        this.html_root = html_root
+
+        this.width = dimensions.width
+        this.height = dimensions.height
+        this.margin = dimensions.margin
+
+        this.init()
+    }
+
+    init() {
+        var svg = d3.select(this.html_root)
+            .append('svg')
+            .attr('width', this.width + this.margin.left + this.margin.right)
+            .attr('height', this.height + this.margin.top + this.margin.bottom)
+
+
+        var view = svg.append('g')
+             .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
+    }
+}
