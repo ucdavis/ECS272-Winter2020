65c6e58ac994029f59d7ec6f1dc858681b9fca19
jacksonvanover <jacksonvanover@gmail.com>
2020-03-01 20:22:27 -0800

    initial commit of embeddings directory; initial commit of pixel_embedding module

---
+++ FinalProject/embeddings/pixel_embedding.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+'''
+In: the dataFrame containing the entire dataset of chart images
+
+Out: the dataFrame containing the entire dataset of chart images with
+a new column of "pixel_embeddings"
+
+resizes each image in the dataFrame to be 100x100 pixels, converts to grayscale so the
+shape is 100x100x1, flattens to an array of length 10000, saves this
+as a "pixel_embedding"
+'''
+def generate_pixel_embeddings(df):
+    return df["pixel_embedding"] = df.apply(lambda x : np.array(x.image.resize((100,100)).convert("L")).flatten(), axis=1)
